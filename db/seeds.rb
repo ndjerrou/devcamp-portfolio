ea314a38b7f52326f78fcc6dabe3777687f202ec
ndjerrou <n.djerrou@gmail.com>
2018-10-21 09:51:02 +0200

    Added fake data via seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create(
+		title: "My Blog Post #{blog}",
+		content: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+		)
+end
+
+puts "10 times blog created"
+
+5.times do |skill|
+	Skill.create(
+		title: "SKill #{skill}",
+		percent_utilized: 5
+		)
+end
+
+puts "5 skills created"
+
+7.times do |portfolio_item|
+
+	Portfolio.create(
+	title: "Portfolio items #{portfolio_item}",
+	subtitle: "My superb services!",
+	body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+	main_image: "https://via.placeholder.com/600x400",
+	thumb_image: "https://via.placeholder.com/350x200"
+	)
+end
+
+puts "9 portfolio items created"
